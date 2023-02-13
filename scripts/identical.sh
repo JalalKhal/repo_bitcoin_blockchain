0a07d80c5cffbde2401470be676880d5c60af689
khaldi <jalalkhaldi3@gmail.com>
2023-02-13 09:55:02 +0100

    update final uncomment code

--- scripts/identical.sh
+++ scripts/identical.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-
-number_files=2000
-
+#store the number of files from the command line argument
+number_files=$1
 for ((i=0; i<number_files; i++))
 do
   for ((j=i+1; j<number_files; j++))
   do
-    file1="../tests/noeud${i}_blockchain.json"
-    file2="../tests/noeud${j}_blockchain.json"
-    diff_files=$(diff "$file1" "$file2")
-    $diff_files > /dev/null 2>&1 #(explications:https://www.security-helpzone.com/2019/09/22/pourquoi-utiliser-dev-null-2-1/)
+    # Store the names of the two files to be compared
+    file1="../noeud${i}_blockchain.json"
+    file2="../noeud${j}_blockchain.json"
+    # Store the difference between the two files
+    diff_files=$(diff "$file1" "$file2") > /dev/null 2>&1 # This is used to suppress any error messages and redirect to /dev/null
     if [ $? -ne 0 ]
     then
       echo "The contents of $file1 and $file2 are different."
       echo $diff_files
-      exit 0
+      exit 1
     fi
   done
 done
 
 echo "The contents of all the files are identical."
-exit 1
+#remove all the files
+rm -r ../noeud*_blockchain.json
+exit 0
 
 
 
